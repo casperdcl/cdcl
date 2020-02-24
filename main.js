688e7ac950ac68866114d7cdd80c98eb2efc00e8
Casper da Costa-Luis <casper.dcl@physics.org>
2020-02-24 00:02:43 +0000

    gist: 5 min auto-refresh

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('div.gist').each(function(){
+    const gist = $(this);
+    const URL = "https://api.github.com/gists/" + gist.text();
+    setInterval(function(){
+      $.get(URL, function(data){
+        const content = Object.values(data.files)[0].content;
+        gist.html(content.split("\n").join("<br/>"));
+      });
+    }, 1000 * 60 * 5);
+  });
+});
